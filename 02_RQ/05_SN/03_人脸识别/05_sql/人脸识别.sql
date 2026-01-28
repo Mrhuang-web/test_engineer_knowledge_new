88b095ab1ee71c9be70bd2da8f7999acf1b502f4
Mrhuang-web <2032327325@qq.com>
2026-01-28 16:39:28 +0800

    version_0.6_补齐

--- 02_RQ/05_SN/03_人脸识别/05_sql/人脸识别.sql
+++ 02_RQ/05_SN/03_人脸识别/05_sql/人脸识别.sql
@@ -6,3 +6,13 @@ access_control_device
 SELECT * FROM entrance_face_work_ord;
 SELECT * FROM entrance_face_success_device;
 
+
+SELECT * from ods_zz_site LIMIT 10
+SELECT * from ods_zz_site_property LIMIT 10
+
+SELECT * FROM t_cfg_fsu WHERE `listen_port` = 10101;
+SELECT * FROM t_cfg_device WHERE device_id = '00800000025'
+
+
+delete FROM entrance_face_work_ord;
+delete FROM entrance_face_success_device;
